d879a26054cf918eb958c28fad1467afa8bf7fa9
Bapon Kar <baponkar600@gmail.com>
2024-06-13 22:41:11 +0530

    Ve Kamleya song added

--- script.js
+++ script.js
@@ -1,109 +1,127 @@
- const musicContainer = document.querySelector('.music-container');
- const playBtn = document.querySelector('#play');
- const prevBtn = document.querySelector('#previous');
- const nextBtn = document.querySelector('#next');
- const audio = document.querySelector('#audio');
- const progress = document.querySelector('.progress');
- const progressContainer = document.querySelector('.progress-container');
- const title = document.querySelector('#title');
- const cover = document.querySelector('#cover');
+const musicContainer = document.querySelector('.music-container');
+const playBtn = document.querySelector('#play');
+const prevBtn = document.querySelector('#previous');
+const nextBtn = document.querySelector('#next');
+const audio = document.querySelector('#audio');
+const progress = document.querySelector('.progress');
+const progressContainer = document.querySelector('.progress-container');
+const title = document.querySelector('#title');
+const cover = document.querySelector('#cover');
 
- /* Song Title Array */
-const songs = ['Mayi teri chunariya lehray -Arijit singh', 
+/* Song Title Array */
+const songs = [
+    'Ve Kamleya -Arijit Singh and Shreya Ghosal',
+    'Mayi teri chunariya lehray -Arijit singh', 
     'Naina -Arijit Singh', 
     'Abhi mujh mein kahin -Sonu Nigam',
     'Bajlo chutir ghonta -Shilajit Majumdar',
     'Holud pakhi -Cactus',
     'Jaane kaise -KK',
-    'Sei tumi Ayub Bachchu',
+    'Sei tumi -Ayub Bachchu',
     '10 hits - B Praak',
     'Ek Bat Batao -B. Praak',
     'Meri Ma -Shankar Mahadevan'
 ];
 
-//Keep track of songs
-let songIndex = getRandomInt(songs.length);
+// Function to get song index from URL parameter
+function getSongIndexFromURL() {
+    const urlParams = new URLSearchParams(window.location.search);
+    return parseInt(urlParams.get('songIndex'), 10);
+}
 
-//load in first song into the player
-loadSong(songs[songIndex]);
+// Keep track of songs
+let songIndex;
 
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
-  
-
-  
-
-//update song details
-function loadSong(song){
+// Load a song into the player
+function loadSong(song) {
     title.innerText = song;
     audio.src = `./music/${song}.mp3`;
     //cover.src = `./images/${song}.jpg`;
-
 }
 
-function playSong(){
+function playSong() {
     musicContainer.classList.add('play');
     playBtn.querySelector('i.fas').classList.remove('fa-play');
     playBtn.querySelector('i.fas').classList.add('fa-pause');
     audio.play();
 }
 
-function pauseSong(){
+function pauseSong() {
     musicContainer.classList.remove('play');
     playBtn.querySelector('i.fas').classList.add('fa-play');
     playBtn.querySelector('i.fas').classList.remove('fa-pause');
     audio.pause();
 }
 
-//Event Listeners
-playBtn.addEventListener('click', () =>{
+// Event Listeners
+playBtn.addEventListener('click', () => {
     const isPlaying = musicContainer.classList.contains('play');
-    if(isPlaying){
+    if (isPlaying) {
         pauseSong();
-    }
-    else{
+    } else {
         playSong();
     }
-})
+});
 
-prevBtn.addEventListener('click', () =>{
+prevBtn.addEventListener('click', () => {
     songIndex--;
-    if(songIndex < 0){
+    if (songIndex < 0) {
         songIndex = songs.length - 1;
     }
     loadSong(songs[songIndex]);
     playSong();
-})
+    updateURL();
+});
 
-nextBtn.addEventListener('click', () =>{
+nextBtn.addEventListener('click', () => {
     songIndex++;
-    if(songIndex > songs.length - 1){
+    if (songIndex > songs.length - 1) {
         songIndex = 0;
     }
     loadSong(songs[songIndex]);
     playSong();
-})
+    updateURL();
+});
 
 audio.addEventListener('timeupdate', (e) => {
-    const {duration, currentTime} = e.srcElement;
-    const progressPercent = (currentTime/duration) * 100;
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
     progress.style.width = `${progressPercent}%`;
-})
+});
 
-progressContainer.addEventListener('click', (e) =>{
+progressContainer.addEventListener('click', (e) => {
     const width = progressContainer.clientWidth;
     const clickX = e.offsetX;
     const duration = audio.duration;
     audio.currentTime = (clickX / width) * duration;
-})
+});
 
 audio.addEventListener('ended', () => {
     songIndex++;
-    if(songIndex > songs.length - 1){
+    if (songIndex > songs.length - 1) {
         songIndex = 0;
     }
     loadSong(songs[songIndex]);
     playSong();
-})
+    updateURL();
+});
+
+// Function to update the URL with the current song index
+function updateURL() {
+    const url = new URL(window.location);
+    url.searchParams.set('songIndex', songIndex);
+    window.history.pushState({}, '', url);
+}
+
+// Initialize player with URL parameter or random song
+const songIndexFromURL = getSongIndexFromURL();
+if (!isNaN(songIndexFromURL) && songIndexFromURL >= 0 && songIndexFromURL < songs.length) {
+    songIndex = songIndexFromURL;
+} else {
+    songIndex = getRandomInt(songs.length);
+}
+loadSong(songs[songIndex]);
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
